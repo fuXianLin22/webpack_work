29001a4d5d4ebc97df1506213f597d0fe95ea1c5
fuXianLin <402240265@qq.com>
2022-06-17 19:36:48 +0800

    finish devServer config

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,21 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/main.js',
-    output:{
-        path:path.join(__dirname,'dist'),
-        filename:'main.js'
+    // mode: 'development || production' production为生产环境，development为开发环境
+    mode: 'development',
+    entry: './src/main.js', //入口文件地址
+    output: {
+        path: path.join(__dirname, 'dist'),  // 出口文件地址
+        filename: 'main.js'  // 出口文件名字
     },
-    plugins:[
-        new HtmlWebpackPlugin()
-    ]
+    devServer: {
+        port: 3000, // 端口号
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',  //配置index.html模板地址
+            filename: 'index.html'  // 生成的文件名字
+        })
+    ],
 }
