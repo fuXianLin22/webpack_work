63c473def4e8f5292179f8a3195ffe085c6f435c
fuXianLin <402240265@qq.com>
2022-06-17 20:05:25 +0800

    finish config loader cssflie and loader lessflie

--- src/main.js
+++ src/main.js
@@ -3,3 +3,7 @@ import { tab } from "./tab";
 
 tab()
 marquee()
+// 引入index.css 
+import './style/index.css'
+// 引入index.less
+import './style/index.less'
--- webpack.config.js
+++ webpack.config.js
@@ -22,4 +22,20 @@ module.exports = {
             filename: 'index.html'  // 生成的文件名字
         })
     ],
+    module: {
+        rules: [ // loader的规则
+            {
+                test: /\.css$/i, // 匹配所有的css文件
+                // use数组里从右向左运行
+                // 先用 css-loader 让webpack能够识别 css 文件的内容并打包
+                // 再用 style-loader 将样式, 把css插入到dom中
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.less$/,
+                // 使用less-loader, 让webpack处理less文件, 内置还会用less翻译less代码成css内容
+                use: ["style-loader", "css-loader", 'less-loader']
+            },
+        ]
+    }
 }
